1e2d0934ce11ea4e574bb23c7e927e42a30b6108
Dorian Taylor <dorian.taylor.lists@gmail.com>
2012-10-20 13:58:46 -0700

    cf http://weblog.bulknews.net/post/33907905561/do-not-ship-modules-with-module-install-1-04

--- lib/URI/cid.pm
+++ lib/URI/cid.pm
@@ -14,11 +14,11 @@ URI::cid - RFC 2392 cid: URI implementation
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
--- lib/URI/mid.pm
+++ lib/URI/mid.pm
@@ -15,11 +15,11 @@ URI::mid - RFC 2392 mid: URI implementation
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
